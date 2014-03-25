98d38bd9933fb350610968ec29fd58eb9abd035a
Michael Slevin <michaelslevin11@gmail.com>
2014-03-24 22:44:26 -0700

    fixed the code, made the README all fancy

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 The Sieve of Eratosthenes
 =========================
 
+**From Wikipedia:**
+>In mathematics, the sieve of Eratosthenes, one of a number of prime number 
+>sieves, is a simple, ancient algorithm for finding all prime numbers up to 
+>any given limit. It does so by iteratively marking as composite (i.e. not 
+>prime) the multiples of each prime, starting with the multiples of 2.
 
+---
 
 An implementation of the Sieve of Eratosthenes in C. 
 
@@ -12,9 +18,9 @@ It also prompts for whether or not you want to print the output. It prints in ch
 beause it prints "when it finds the primes".
 
 The program also gives you the calculation time- this will be much higher if
-you print. You can see some commented out code- feel free to remove the prints
-in the algorithm and un-comment the other prints to print the primes independently
-of calculation.
+you print the results. Of course, then its somewhat useless.
+
+*There was another printing method in there, but I removed it because I'm an idiot.*
 
 It segfaults for me when I try numbers near a billion- probably just because calculating
 primes below a billion would take +3GB just to hold the data (stored as longs).
--- sieve.c
+++ sieve.c
@@ -4,19 +4,19 @@
 #include <time.h>
 
 typedef struct Number {
-   long num;
+   int num;
    int true; // true = prime/unchecked, false = non-prime
 } Number;
 
 int main() {
-   long max, i, j, mult = 1;
-   int print;
+   int max, mult = 1;
+   int print, i, j;
    Number *list;
    clock_t begin, end;
    double time;
 
    printf("Enter a max number: ");
-   scanf("%ld", &max);
+   scanf("%d", &max);
    printf("Do you want to print the output? (1 = yes, 0 = no): ");
    scanf("%d", &print);
 
@@ -29,23 +29,19 @@ int main() {
 
    begin = clock(); // for timing
 
-
    /* This is the actual algorithm!
     * Printing stuff is included here as well */
    for(i = 2; i < sqrt(max); i++) {
       if(list[i].true) {
-         if(print) printf("%ld ", list[i].num);
          for(j = i * i; j < max; j = (i * i) + (mult++ * i)) {
             list[j].true = 0;
          }
          mult = 1;
       }
    }
-   if(print) printf("\n");
    
    end = clock(); // for timing
    
-   /*
    if(print) {
       printf("\nPrime numbers below %d:\n", max);
       for(i = 0; i < max; i++) {
@@ -55,7 +51,7 @@ int main() {
       }  
       printf("\n");
    }   
-   */
+   
    time = (double)(end - begin) / CLOCKS_PER_SEC;
 
    printf("Calculation time: %.4lf seconds\n", time);
