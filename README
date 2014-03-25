7f370b8bb2be2df3ad86f20b476f7fd4a39e39ce
Michael Slevin <michaelslevin11@gmail.com>
2014-03-24 21:57:40 -0700

    initial commit

---
+++ README
@@ -0,0 +1,17 @@
+An implementation of the Sieve of Eratosthenes in C. 
+
+To use: compile (use -lm with gcc) and run the executable. The pogram will 
+calculate primes up to (non-inclusive) the entered number.
+
+It also prompts for whether or not you want to print the output. It prints in chunks
+beause it prints "when it finds the primes".
+
+The program also gives you the calculation time- this will be much higher if
+you print. You can see some commented out code- feel free to remove the prints
+in the algorithm and un-comment the other prints to print the primes independently
+of calculation.
+
+It segfaults for me when I try numbers near a billion- probably just because calculating
+primes below a billion would take +3GB just to hold the data (stored as longs).
+
+This program isn't super fast, but I love the algorithm. 
---
+++ sieve.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+typedef struct Number {
+   long num;
+   int true; 
+} Number;
+
+int main() {
+   long max, i, j, mult = 1;
+   int print;
+   Number *list;
+   clock_t begin, end;
+   double time;
+
+   printf("Enter a max number: ");
+   scanf("%ld", &max);
+   printf("Do you want to print the output? (1 = yes, 0 = no): ");
+   scanf("%d", &print);
+
+   list = malloc(sizeof(Number) * max);
+
+   for(i = 0; i < max; i++) {
+      list[i].num = i;
+      list[i].true = 1;
+   }  
+
+   begin = clock();
+   for(i = 2; i < sqrt(max); i++) {
+      if(list[i].true) {
+         if(print) printf("%ld ", list[i].num);
+         for(j = i * i; j < max; j = (i * i) + (mult++ * i)) {
+            list[j].true = 0;
+         }
+         mult = 1;
+      }
+   }
+   if(print) printf("\n");
+   end = clock();
+   
+   /*
+   if(print) {
+      printf("\nPrime numbers below %d:\n", max);
+      for(i = 0; i < max; i++) {
+         if(list[i].true) {
+            printf("%d ", list[i].num);
+         }
+      }  
+      printf("\n");
+   }   
+   */
+   time = (double)(end - begin) / CLOCKS_PER_SEC;
+
+   printf("Calculation time: %.4lf seconds\n", time);
+
+   return 0;
+}
